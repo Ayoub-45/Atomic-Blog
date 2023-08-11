ea52072fdd5f558da5ce35523773553a583a4a27
unknown <ayoubnasraoui161@yahoo.fr>
2023-08-11 19:54:37 +0200

    Add custom hook

--- src/PostContext.js
+++ src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { faker } from "@faker-js/faker";
 function createRandomPost() {
     return {
@@ -6,32 +6,31 @@ function createRandomPost() {
         body: faker.hacker.phrase(),
     };
 }
-const PostContext=createContext();
-function PostProvider({children}){
+const PostContext = createContext();
+function PostProvider({ children }) {
     const [posts, setPosts] = useState(() =>
-    Array.from({ length: 30 }, () => createRandomPost())
-);
-const [searchQuery, setSearchQuery] = useState("");
+        Array.from({ length: 30 }, () => createRandomPost())
+    );
+    const [searchQuery, setSearchQuery] = useState("");
+    // Derived state. These are the posts that will actually be displayed
+    const searchedPosts =
+        searchQuery.length > 0
+            ? posts.filter((post) =>
+                  `${post.title} ${post.body}`
+                      .toLowerCase()
+                      .includes(searchQuery.toLowerCase())
+              )
+            : posts;
 
-// Derived state. These are the posts that will actually be displayed
-const searchedPosts =
-    searchQuery.length > 0
-        ? posts.filter((post) =>
-              `${post.title} ${post.body}`
-                  .toLowerCase()
-                  .includes(searchQuery.toLowerCase())
-          )
-        : posts;
+    function handleAddPost(post) {
+        setPosts((posts) => [post, ...posts]);
+    }
 
-function handleAddPost(post) {
-    setPosts((posts) => [post, ...posts]);
-}
-
-function handleClearPosts() {
-    setPosts([]);
-}
-return (
-    <PostContext.Provider
+    function handleClearPosts() {
+        setPosts([]);
+    }
+    return (
+        <PostContext.Provider
             value={{
                 posts: searchedPosts,
                 onAddPost: handleAddPost,
@@ -41,8 +40,14 @@ return (
             }}
         >
             {children}
-     </PostContext.Provider>
-        
-)
+        </PostContext.Provider>
+    );
 }
-export {PostProvider,PostContext}
+function usePosts() {
+    const context = useContext(PostContext);
+    if (context === undefined) {
+        throw new Error("PostContext was used outside of the PostProvider");
+    }
+    return context;
+}
+export { PostProvider, PostContext, usePosts };
