3a082e425dbba9067a07a8d6f151c6c2ae21007b
unknown <ayoubnasraoui161@yahoo.fr>
2023-08-21 18:22:08 +0200

    Understand the memo function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState ,memo} from "react";
 import { faker } from "@faker-js/faker";
 import {PostProvider,usePosts} from "./PostContext";
-import Test from "./Test";
-
 function createRandomPost() {
     return {
         title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
@@ -13,8 +11,8 @@ function createRandomPost() {
 function App() {
    
     // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
-    const [isFakeDark, setIsFakeDark] = useState(false);
-useEffect(
+   const [isFakeDark, setIsFakeDark] = useState(false);
+useEffect (
         function () {
             document.documentElement.classList.toggle("fake-dark-mode");
         },
@@ -28,7 +26,7 @@ useEffect(
         <PostProvider>
                 <Header />
                 <Main />
-                <Archive />
+                <Archive show={false}/>
                 <Footer />
         </PostProvider>
             </section>
@@ -138,20 +136,19 @@ function List() {
                 </li>
             ))}
         </ul>
-        <Test/>
+     
             </>
     );
 }
 
-function Archive() {
-    const { onAddPost } =usePosts()
+const Archive=memo( function Archive({show}) {
     // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
     const [posts] = useState(() =>
         // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
-        Array.from({ length: 10000 }, () => createRandomPost())
+        Array.from({ length: 20000 }, () => createRandomPost())
     );
 
-    const [showArchive, setShowArchive] = useState(false);
+    const [showArchive, setShowArchive] = useState(show);
 
     return (
         <aside>
@@ -167,17 +164,13 @@ function Archive() {
                             <p>
                                 <strong>{post.title}:</strong> {post.body}
                             </p>
-                            <button onClick={() => onAddPost(post)}>
-                                Add as new post
-                            </button>
                         </li>
                     ))}
                 </ul>
             )}
         </aside>
     );
-}
-
+})
 function Footer() {
     return <footer>&copy; by The Atomic Blog ✌️</footer>;
 }
